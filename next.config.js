d7cf266bdbb5888247b6b4fd7fb01333897fa98a
Alvin Chau <alvinchau2013@gmail.com>
2022-01-25 00:47:01 -0500

    added secrets

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   reactStrictMode: true,
   target: 'experimental-serverless-trace',
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    SECRET: process.env.SECRET,
+  }
 }
