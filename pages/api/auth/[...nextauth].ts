23b61ce75d9cf1c61d7366faabf8929954f5d75e
Alvin Chau <alvinchau2013@gmail.com>
2022-01-25 00:28:20 -0500

    added TS

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -104,7 +104,7 @@ export default NextAuth({
   // pages is not specified for that route.
   // https://next-auth.js.org/configuration/pages
   pages: {
-    signIn: '/signin',  // Displays signin buttons
+   // Displays signin buttons
     // signOut: '/auth/signout', // Displays form with sign out button
     // error: '/auth/error', // Error code passed in query string as ?error=
     // verifyRequest: '/auth/verify-request', // Used for check email page
